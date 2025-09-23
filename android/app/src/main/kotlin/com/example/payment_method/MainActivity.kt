3dcab8f41829b96efef9840dfa0a9d24b65ea9f0
NamithaNaik <namithanaik50@gmail.com>
2025-09-23 15:53:08 +0530

    updated android files

--- android/app/src/main/kotlin/com/example/payment_method/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/payment_method/MainActivity.kt
@@ -1,5 +1,4 @@
-package com.example.payment_method
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
