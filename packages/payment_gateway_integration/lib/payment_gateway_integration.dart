dd41d261a8cc5ef5033fe04551dbefa43b8d3f30
NamithaNaik <namithanaik50@gmail.com>
2025-09-24 12:59:29 +0530

    feat: remove stripe payment provider

--- packages/payment_gateway_integration/lib/payment_gateway_integration.dart
+++ packages/payment_gateway_integration/lib/payment_gateway_integration.dart
@@ -9,5 +9,4 @@ export 'src/core/payment_manager.dart';
 // Providers
 export 'src/razorpay/razorpay_options.dart';
 export 'src/razorpay/razorpay_payment_service.dart';
-export 'src/stripe/stripe_options.dart';
-export 'src/stripe/stripe_payment_service.dart';
+
